88e6445cd4469f998db73444520378cb81be116d
satoyan <satoyan.discord@gmail.com>
2024-07-11 10:26:40 +0900

    イマージタブ作成

---
+++ react-sample/src/components/image_tab/ImageTab.tsx
@@ -0,0 +1,9 @@
+import { useRef, useState } from "react";
+import { formElementsTypes } from "../../formElementTypes";
+import { propsType } from "../../propsType";
+
+export const ImageTab: React.FC<{
+	props: propsType;
+}> = ({ props }) => {
+	return <></>;
+};
