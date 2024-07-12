f0a3085e0d64480c3fcec6e41e979fe7535de8a2
satoyan <satoyan.discord@gmail.com>
2024-07-12 18:48:47 +0900

    全て削除のボタン追加

--- react-sample/src/components/image_tab/ImageTab.tsx
+++ react-sample/src/components/image_tab/ImageTab.tsx
@@ -12,6 +12,20 @@ export const ImageTab: React.FC<{
 	return (
 		<>
 			<Dropzone props={{ ...props }} />
+			{Object.keys(props.uploadedImages).length !== 0 ? (
+				<div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+					<button
+						style={{ width: "calc(100% - 10px)" }}
+						onClick={(e) => {
+							props.setUploadedImages({});
+						}}
+					>
+						全て削除
+					</button>
+				</div>
+			) : (
+				<></>
+			)}
 			<ImageList props={{ ...props }} />
 		</>
 	);
