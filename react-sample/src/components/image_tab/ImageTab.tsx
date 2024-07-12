5dc08ab42087627a0a62df1c348f82b6eeb05c9c
satoyan <satoyan.discord@gmail.com>
2024-07-12 11:20:39 +0900

    ドロップゾーンを別ファイルに切り分け
    クリックでファイル追加機能を削除

--- react-sample/src/components/image_tab/ImageTab.tsx
+++ react-sample/src/components/image_tab/ImageTab.tsx
@@ -3,74 +3,14 @@ import { formElementsTypes } from "../../formElementTypes";
 import { propsType } from "../../propsType";
 import React from "react";
 import { themeColors } from "../themeColor";
+import { Dropzone } from "./Dropzone";
 
-function arrayBufferToBase64(buffer: ArrayBuffer) {
-	var binary = "";
-	var bytes = new Uint8Array(buffer);
-	var len = bytes.byteLength;
-	for (var i = 0; i < len; i++) {
-		binary += String.fromCharCode(bytes[i]);
-	}
-	return window.btoa(binary);
-}
 export const ImageTab: React.FC<{
 	props: propsType;
 }> = ({ props }) => {
 	return (
 		<>
-			<div
-				style={{
-					width: "calc(100% - 14px)",
-					height: "50px",
-					margin: "5px",
-					backgroundColor: themeColors[props.themeColor].dropzone.background,
-					border: "solid 2px black",
-					display: "flex",
-					alignContent: "center",
-					justifyContent: "center",
-				}}
-				onDragOver={(e) => e.preventDefault()}
-				onDrop={async (e) => {
-					e.preventDefault();
-					const items = Array.from(e.dataTransfer.items);
-
-					const base64_images = await Promise.all(
-						items.map(
-							(item, i) =>
-								new Promise<{ base64: string; path: string }[]>(async (resolve) => {
-									const entry = item.webkitGetAsEntry();
-									if (!entry) return;
-									const file_entries = await searchFile(entry);
-									const getFile = (file_entry: FileSystemFileEntry) => {
-										return new Promise<{ file: File; path: string }>((resolve, reject) => {
-											file_entry.file(
-												(file) => resolve({ file: file, path: file_entry.fullPath.slice(1) }),
-												(e) => reject(e)
-											);
-										});
-									};
-									const files = await Promise.all(file_entries.map((f) => getFile(f)));
-									const base64_images = await Promise.all(
-										files.map(async ({ file, path }) => {
-											if (file.type !== "image/png" && file.type !== "image/jpeg") return { path: "", base64: "" };
-											return { path, base64: arrayBufferToBase64(await file.arrayBuffer()) };
-										})
-									);
-									resolve(base64_images.filter((v) => v.path !== ""));
-								})
-						)
-					);
-					const result: { [path: string]: string } = props.uploadingImages;
-					for (let uploading_image of base64_images.flat()) {
-						result[uploading_image.path] = uploading_image.base64;
-					}
-					props.setUploadingImages(result);
-				}}
-				onClick={(e) => (e.target as HTMLDivElement).querySelector("input")?.click()}
-			>
-				<input type="file" accept="image/png, image/jpeg" hidden />
-				<p style={{ pointerEvents: "none", userSelect: "none" }}>ここにドロップしてファイルまたはフォルダをアップロード</p>
-			</div>
+			<Dropzone props={props} />
 			<div
 				style={{
 					display: "flex",
@@ -128,32 +68,3 @@ export const ImageTab: React.FC<{
 		</>
 	);
 };
-
-async function searchFile(entry: FileSystemEntry): Promise<FileSystemFileEntry[]> {
-	if (entry.isFile) {
-		return [entry as FileSystemFileEntry];
-	} else if (entry.isDirectory) {
-		const dirReader = (entry as FileSystemDirectoryEntry).createReader();
-		const getEntries = () =>
-			new Promise<FileSystemEntry[]>((resolve) => {
-				dirReader.readEntries((entries) => {
-					resolve(entries);
-				});
-			});
-		let all_entries: FileSystemEntry[] = [];
-		const readAllEntries = async () => {
-			const entries = await getEntries();
-			if (entries.length > 0) {
-				all_entries = [...all_entries, ...entries];
-				await readAllEntries();
-			}
-		};
-		await readAllEntries();
-		let result_paths: FileSystemFileEntry[] = [];
-		for (const entry of all_entries) {
-			result_paths.push(...(await searchFile(entry)));
-		}
-		return result_paths;
-	}
-	return [];
-}
