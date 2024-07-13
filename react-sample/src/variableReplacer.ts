8ba456aeee3f578f372e0e7e6d477fe9a1d156e5
satoyan <satoyan.discord@gmail.com>
2024-07-13 21:43:46 +0900

    文字を置換する関数追加

---
+++ react-sample/src/variableReplacer.ts
@@ -0,0 +1,22 @@
+export const variableReplacer = (original_text: string, variables: { [key: string]: string | number | boolean }) => {
+	const split_texts = original_text.split("%");
+	let count = 0;
+	const variable_texts: string[] = [];
+	for (let split_text of split_texts) {
+		count += 1;
+		if (split_text.endsWith("\\")) {
+			count -= 1;
+		}
+		if (count === 2) {
+			variable_texts.push(split_text);
+			count = 0;
+		}
+	}
+
+	let target_text = String(original_text);
+	for (let variable_text of variable_texts) {
+		if (variables[variable_text] === undefined) continue;
+		target_text = target_text.replace(`%${variable_text}%`, String(variables[variable_text]));
+	}
+	return target_text.replace("%", "").replace("\\%", "%");
+};
