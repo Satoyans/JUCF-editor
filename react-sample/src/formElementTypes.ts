53c37980f4ebd3285f697848e9725a707d323937
satoyan <satoyan.discord@gmail.com>
2024-07-14 00:43:47 +0900

    変数用のフォームエレメントの型作成

--- react-sample/src/formElementTypes.ts
+++ react-sample/src/formElementTypes.ts
@@ -7,23 +7,16 @@ export namespace formElementsTypes {
 		label?: string;
 	}
 	export namespace elementPropertiesOption {
-		export interface buttonOption {
-			// is_show_button?: boolean;
-		}
-		export interface closeButtonOption {
-			// is_show_close?: boolean;
-		}
+		export interface buttonOption {}
+		export interface closeButtonOption {}
 		export interface textOption {
 			text: string;
-			// is_show_text?: boolean;
 		}
 		export interface imageOption {
 			texture: string;
-			// is_show_image?: boolean;
 		}
 		export interface hoverTextOption {
 			hover_text: string;
-			// is_show_hover?: boolean; //実際は使っていない、falseならhover_textを""にするだけ
 		}
 		export interface customOption {
 			buttonOption?: buttonOption;
@@ -50,6 +43,55 @@ export namespace formElementsTypes {
 			is_show_close: boolean;
 			is_show_text: boolean;
 			is_show_image: boolean;
+		}
+	}
+}
+
+export namespace formElementsVariableTypes {
+	interface basePropertiesType {
+		w: string; //変数使えるように
+		h: string; //変数使えるように
+		x: string; //変数使えるように
+		y: string; //変数使えるように
+		label?: string;
+	}
+	export namespace elementPropertiesOption {
+		export interface buttonOption {}
+		export interface closeButtonOption {}
+		export interface textOption {
+			text: string;
+		}
+		export interface imageOption {
+			texture: string;
+		}
+		export interface hoverTextOption {
+			hover_text: string;
+		}
+		export interface customOption {
+			buttonOption?: buttonOption;
+			closeButtonOption?: closeButtonOption;
+			textOption?: textOption;
+			imageOption?: imageOption;
+			hoverTextOption?: hoverTextOption;
+		}
+	}
+	export namespace elementPropertiesTypes {
+		export interface addButton extends basePropertiesType, elementPropertiesOption.buttonOption {}
+		export interface addCloseButton extends basePropertiesType, elementPropertiesOption.closeButtonOption {}
+		export interface addText extends basePropertiesType, elementPropertiesOption.textOption {}
+		export interface addImage extends basePropertiesType, elementPropertiesOption.imageOption {}
+		export interface addHoverText extends basePropertiesType, elementPropertiesOption.hoverTextOption {}
+		export interface all
+			extends basePropertiesType,
+				elementPropertiesOption.buttonOption,
+				elementPropertiesOption.closeButtonOption,
+				elementPropertiesOption.hoverTextOption,
+				elementPropertiesOption.imageOption,
+				elementPropertiesOption.textOption {
+			is_show_button: string; //変数
+			is_show_close: string; //変数
+			is_show_text: string; //変数
+			is_show_image: string; //変数
 		}
 	}
 }
