a9d26aa6f3070ee129a4d45ab1f15a463f900042
satoyan <satoyan.discord@gmail.com>
2024-07-15 16:05:55 +0900

    変数追加機能作成

--- react-sample/src/components/variable_tab/VariableInput.tsx
+++ react-sample/src/components/variable_tab/VariableInput.tsx
@@ -1,7 +1,39 @@
-export const VariableInput: React.FC<{ props: {} }> = ({ props }) => {
+export const VariableInput: React.FC<{
+	props: {
+		variable: {
+			[key: string]: string | number | boolean;
+		};
+		setVariable: React.Dispatch<
+			React.SetStateAction<{
+				[key: string]: string | number | boolean;
+			}>
+		>;
+	};
+}> = ({ props }) => {
 	return (
-		<div style={{ margin: "5px", backgroundColor: "#eeeeee", height: "100px", border: "solid 1px black" }}>
-			<input style={{ width: "calc(100% - 20px)" }} type="text" />
+		<div style={{ margin: "5px", marginBottom: "10px", height: "40px", border: "solid 2px black", display: "flex", alignItems: "center", justifyContent: "center" }}>
+			<input id="variable_input_key" style={{ width: "20%", height: "20px", margin: "5px" }} type="text" autoComplete="off" />
+			<span>:</span>
+			<input id="variable_input_value" style={{ width: "20%", height: "20px", margin: "5px" }} type="text" autoComplete="off" />
+			<button
+				style={{ width: "60px" }}
+				onClick={(e) => {
+					const input_key_input = document.querySelector("#variable_input_key")! as HTMLInputElement;
+					const input_value_input = document.querySelector("#variable_input_value")! as HTMLInputElement;
+
+					const input_key = input_key_input.value;
+					const input_value = input_value_input.value;
+					if (input_key === "") return;
+					if (props.variable[input_key] !== undefined) return;
+					props.setVariable({ ...props.variable, [input_key]: input_value });
+
+					//リセット
+					input_key_input.value = "";
+					input_value_input.value = "";
+				}}
+			>
+				追加
+			</button>
 		</div>
 	);
 };
