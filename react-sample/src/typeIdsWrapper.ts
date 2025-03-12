d49ec7830d3d09f9a5382aa2cd9c3a378d4172e4
satoyan <satoyan.discord@gmail.com>
2025-03-12 22:24:37 +0900

    typeIdを更新
    BPと統一

--- react-sample/src/typeIdsWrapper.ts
+++ react-sample/src/typeIdsWrapper.ts
@@ -1,7 +1,4 @@
-import { typeIdToDataId, typeIdToID } from "./typeIds.js";
+import { typeIdToID } from "./typeIds";
 
 //react-selectでoptionsとして使うための形に変換する
-export const typeIdOptions = [
-	...Array.from(typeIdToDataId.keys()).map((key) => ({ label: key, value: typeIdToDataId.get(key) })),
-	...Array.from(typeIdToID.keys()).map((key) => ({ label: key, value: typeIdToID.get(key) })),
-];
+export const typeIdOptions = [...Array.from(typeIdToID.keys()).map((key) => ({ label: key, value: typeIdToID.get(key) }))];
